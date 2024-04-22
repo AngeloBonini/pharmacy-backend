5e5145952b66a726e0cc035eefd7a0708478a081
Angelo Monte Serrat Bonini <abonini2000@gmail.com>
2024-04-22 13:22:59 -0300

    (feat): setting intial database connection

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const db = require("./db");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/users", async (req, res) => {
+  try {
+    const { rows } = await db.query("SELECT * FROM users");
+    res.send(rows);
+  } catch (err) {
+    res.status(500).send("Server error");
+  }
 });
 
 app.listen(port, () => {
